6fe40e4748d08e3e7a9dc822630cb4bfd48be654
watermelon <fekihfarouk88@gmail.com>
2024-11-20 18:34:26 +0100

    Decrypt Functionality

--- src/GoatCrypter.java
+++ src/GoatCrypter.java
@@ -1,5 +1,5 @@
 class GoatCrypter{
-	private final String standardKeyword = "G.O.A.T";
+	private final String standardKeyword = "6543r2@2391-d,,,.^^";
 	public String keyword;
 	public String word;
 	public String result;
@@ -42,9 +42,43 @@ class GoatCrypter{
 		}
 	}
 	
+	public void decrypt() {
+		String decrypted = "";
+		int keywordLength = keyword.length();
+		int wordLength = word.length();
+		int keywordIndex = 0;
+		
+		for(int i=0; i<wordLength; i++) {
+			if(keywordIndex > keywordLength) {
+				keywordIndex = 0;
+			}
+			
+			decrypted += subtractAFromB(word.charAt(i), keyword.charAt(keywordIndex));
+		}
+		
+		this.result = decrypted;
+	}
+	
+	public char subtractAFromB(char a, char b) {
+		int asciiA = a;
+		int asciiB = b;
+		int sum = asciiA - asciiB;
+		
+		if(sum < 32) {
+			return (char) (sum+95);
+		} else {
+			return (char) (sum);
+		}
+		
+	}
+	
 	public static void main(String[] args) {
-		GoatCrypter word1 = new GoatCrypter("Farouk");
+		GoatCrypter word1 = new GoatCrypter("FaroukFekih21wd341@-d.saD3/^^");
 		word1.crypt();
 		System.out.println(word1.result);
+		
+		GoatCrypter word2 = new GoatCrypter("|8IFLB|<B@?hgN;ijgvc;dJ8zie55");
+		word2.decrypt();
+		System.out.println(word2.result);
 	}
 }
